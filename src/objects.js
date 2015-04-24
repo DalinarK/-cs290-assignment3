f59d51624784487de06125e419aa3c18cea3c365
Dustin Dinh <djdinh@gmail.com>
2015-04-23 23:36:14 -0700

    finished log

--- src/objects.js
+++ src/objects.js
@@ -155,3 +155,35 @@ myLog.logMessage("bar", 1);
 myLog.logMessage("baz", 1);
 
 //end your code
+
+
+ function makeRequest(url) 
+ {
+    if (window.XMLHttpRequest) 
+    { // Mozilla, Safari, ...
+      httpRequest = new XMLHttpRequest();
+    } 
+    else if (window.ActiveXObject) 
+    { // IE
+      try 
+      {
+        httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+      } 
+      catch (e) 
+      {
+        try 
+        {
+          httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+        } 
+        catch (e) {}
+      }
+    }
+
+    if (!httpRequest) {
+      alert('Giving up :( Cannot create an XMLHTTP instance');
+      return false;
+    }
+    httpRequest.onreadystatechange = alertContents;
+    httpRequest.open('GET', url);
+    httpRequest.send();
+  }
